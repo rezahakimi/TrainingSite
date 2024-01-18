51be09cae71b25783504c3fbb532dd9d22a557b5
rezahakimi <rezahakimi.eng@gmail.com>
2024-01-18 20:12:06 +0330

    20240118

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -5,10 +5,11 @@ import db from "../models/index.js";
 const connectDB = async () => {
   db.mongoose
     .connect(
-      `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
+      //`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
+      "mongodb+srv://rezahakimi:publickey@cluster0.48qli8k.mongodb.net/?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
       }
     )
     .then(() => {
