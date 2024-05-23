2f97cee3697c173857fbec1afd23f4e9c740fc20
rezahakimi <rezahakimi.eng@gmail.com>
2024-05-23 19:13:08 +0330

    2024052304

--- backend/controllers/articleController.js
+++ backend/controllers/articleController.js
@@ -581,12 +581,13 @@ const getArticlesWithNotAcceptPost = asyncHandler(async (req, res) => {
 
   const myUsersDisLike = await Promise.all(
     articles.map(async (a) => {
+      //const mya = await Article.findById(a.id)
       a.comments.map(async (c) => {
         //   const ac = await ArticleComment.findById(c.commentId)
         // .exec();
         //if (ac.accept === false)
         return {
-          id: c._id,
+          id: a.comments,
         };
       });
     })
